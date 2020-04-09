9680dbf6fd55a2388c087782849e7c64a1dd88d4
Faraz Noor <faraznoor75@gmail.com>
2020-04-09 23:46:01 +0530

    Added Ransack Search and Made table cells uniform

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,9 @@
 class HomeController < ApplicationController
 	def index
-		@q = Vehicle.ransack(params[:q])
-		@vehicles = @q.result(distinct: true).page(params[:page]).per(10)
-		
+		@search = Vehicle.ransack(params[:q])
+		@vehicles = @search.result(distinct: true).page(params[:page]).per(10)
+		@search.build_condition if @search.conditions.empty?
+  		@search.build_sort if @search.sorts.empty?
 		respond_to do |format|
 			format.html
 			format.js
